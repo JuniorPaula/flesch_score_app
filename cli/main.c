db157a20941cbf32d1a7088a82f799be921e52df
Junior Paula <luke.junnior@icloud.com>
2025-09-17 10:14:14 -0300

    Create `C CLI` for using rust function

---
+++ cli/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+extern double flesch_score(const char* text, size_t len);
+
+char* read_file(const char* path)
+{
+  FILE *f = fopen(path, "rb");
+  if (!f) return NULL;
+
+  fseek(f, 0, SEEK_END);
+  long size = ftell(f);
+  rewind(f);
+
+  char* buffer = malloc(size + 1);
+  fread(buffer, 1, size, f);
+  buffer[size] = '\0';
+  fclose(f);
+
+  return buffer;
+}
+
+int main(int argc, char** argv)
+{
+  if (argc < 2) {
+    printf("Uso: ./app arquivo.txt\n");
+    return 1;
+  }
+
+  char* text = read_file(argv[1]);
+  if (!text) {
+    printf("[ERROR] O arquivo não pode ser aberto.\n");
+    return 1;
+  }
+
+  double score = flesch_score(text, strlen(text));
+
+  printf("-- Lib sendo executada a partir da Implementação em C ---\n");
+  printf("\nFlesch Score: %.2f\n", score);
+
+  return 0;
+}
