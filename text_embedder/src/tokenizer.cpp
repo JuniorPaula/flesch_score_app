564c87c501a1ab61a5e0968c40cddca5c7390cdc
Junior Paula <luke.junnior@icloud.com>
2025-10-08 16:29:04 -0300

    Add Stopwords

--- text_embedder/src/tokenizer.cpp
+++ text_embedder/src/tokenizer.cpp
@@ -1,5 +1,24 @@
 #include <utf8proc.h>
+#include <unordered_set>
 #include "../include/tokenizer.hpp"
+
+const std::unordered_set<std::string> STOPWORDS = {
+  "de","a","o","que","e","do","da","em","um","para","e","com","nao","uma","os","no","se","na","por","mais","as","dos","como",
+  "mas","foi","ao","ele","das","tem","a","seu","sua","ou","ser","quando","muito","ha","nos","ja","esta","eu","tambem","so",
+  "pelo","pela","ate","isso","ela","entre","era","depois","sem","mesmo","aos","ter","seus","quem","nas","me","esse","eles",
+  "estao","voce","tinha","foram","essa","num","nem","suas","meu","as","minha","tem","numa","pelos","elas","havia","seja",
+  "qual","sera","nos","tenho","lhe","deles","essas","esses","pelas","este","fosse","dele","tu","te","voces","vos",
+  "lhes","meus","minhas","teu","tua","teus","tuas","nosso","nossa","nossos","nossas","dela","delas","esta","estes",
+  "estas","aquele","aquela","aqueles","aquelas","isto","aquilo","estou","esta","estamos","estao","estive","esteve",
+  "estivemos","estiveram","estava","estavamos","estavam","estivera","estiveramos","esteja","estejamos","estejam","estivesse",
+  "estivessemos","estivessem","estiver","estivermos","estiverem","hei","ha","havemos","hao","houve","houvemos","houveram",
+  "houvera","houveramos","haja","hajamos","hajam","houvesse","houvessemos","houvessem","houver","houvermos","houverem",
+  "houverei","houvera","houveremos","houverao","houveria","houveriamos","houveriam","sou","somos","sao","era","eramos","eram",
+  "fui","foi","fomos","foram","fora","foramos","seja","sejamos","sejam","fosse","fossemos","fossem","for","formos","forem",
+  "serei","sera","seremos","serao","seria","seriamos","seriam","tenho","tem","temos","tem","tinha","tinhamos","tinham","tive",
+  "teve","tivemos","tiveram","tivera","tiveramos","tenha","tenhamos","tenham","tivesse","tivessemos","tivessem","tiver",
+  "tivermos","tiverem","terei","tera","teremos","terao","teria","teriamos","teriam"
+};
 
 std::string normalize(const std::string& input)
 {
@@ -47,13 +66,18 @@ std::vector<std::string> tokenizer(const std::string& text)
     if (std::isalnum(static_cast<unsigned char>(ch))) {
       word += ch; // já está normalizado
     } else if (!word.empty()) {
-      tokens.push_back(word);
+      // filtro de stopwords
+      if (STOPWORDS.find(word) == STOPWORDS.end()) {
+        tokens.push_back(word); 
+      }
       word.clear();
     }
   }
 
   if (!word.empty()) {
-    tokens.push_back(word);
+  if (STOPWORDS.find(word) == STOPWORDS.end()) {
+      tokens.push_back(word); 
+    }
   }
 
   return tokens;
